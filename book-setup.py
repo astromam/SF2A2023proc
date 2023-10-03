031de7a394b8a8b9b2d424d0ffb00d6e60d8daeb
Mamadou <mamadou.ndiaye@oca.eu>
2023-10-03 18:14:25 +0200

    update book-setup script for 2023

--- book-setup.py
+++ book-setup.py
@@ -9,10 +9,10 @@
 # updated by R. Samadi 22.10.2010
 #
 
-year ='2022'
+year ='2023'
 
 bstfile = 'aa.bst'
-clsfile = 'sf2a-conf2022'
+clsfile = 'sf2a-conf2023'
 
 # the place where the proceedings will be installed
 #urlbase_proc="http://lesia.obspm.fr/semaine-sf2a/2012/proceedings/"
@@ -20,54 +20,60 @@ clsfile = 'sf2a-conf2022'
 #urlbase_proc="http://sf2a.cesr.fr/"
 urlbase_proc="http://proceedings.sf2a.eu/" + year + "/"
 
-editeurs='J. Richard, A. Siebert, E. Lagadec, N. Lagarde, O. Venot, J. Malzac, J.-B. Marquette, M. N\'Diaye, D. Briot'
+editeurs='M. N\'Diaye, A. Siebert, N. Lagarde, O. Venot, K. Bailli\'e, M. B\'ethermin, E. Lagadec, J. Malzac, J. Richard'
 
 start_page =1
 
 NOCONTRIB=0 # 1 -> the contributions are not inserted, only the chapters are processed
 
-# definition of the 2022 sessions and contribution
+# definition of the 2023 sessions and contribution
 # sort out contributions according to Review, Talk, Posters
 # (alphabetical order within each subclass)
 # comment out sessions with no contribution
-S0 = ['S0','Chauvin_S0','Douspis_S0','Knodlseder_S0','Meunier_S0','Ouazzani_S0','Robert_S0']
-S1 = ['S1','Boughelilba_S1','DehiwalageDon_S1','ElMellah_S1','Freour_S1','Soudais_S1']
-S2 = ['S2','Amard_S2','Cointepas_S2','Dhouib_S2','ElMellah_S2','Maimone_S2','Merle_S2','Meunier_S2','Noraz_S2','Tabone_S2','Thomassona_S2','Thomassonb_S2']
-S3 = ['S3','LeGouellec_S3','Schirmer_S3','Taillard_S3']
-S4 = ['S4','Boisse_S4','Vienne_S4']
-S5 = ['S5','Alkan_S5','Desmars_S5','Robert_S5','Thuillot_S5','Turpin_S5','Delisle_S5','LeCam_S5','Lekic_S5','Marquette_S5','Wullaert_S5']
-S6 = ['S6','Webb_S6']
-S7 = ['S7','Freour_S7']
-S9 = ['S9','Pitout_S9']
-S10 = ['S10','Galliano_S10','SeillÃ©_S10']
-S11 = ['S11','Breton_S11','Delorme_S11']
-S13 = ['S13','Berteaud_S13','Bourgoin_S13','Bruel_S13','Bugli_S13','Contini_S13','Godet_S13','Mignon-Risse_S13','Srinivasan_S13','Webb_S13']
-S14 = ['S14','Tresse_S14']
-S15 = ['S15','Lemasle_S15']
-S16 = ['S16','Dyrek_S16','Malin_S16','Acuna_S16','Desgrange_S16','Ducreux_S16','Jaziri_S16']
-S18 = ['S18','Beust_S18','Griveaud_S18','Gry_S18','Benseguane_S18','Cabral_S18','Crida_S18','Gkotsinas_S18','Martel_S18','Michoulier_S18']
-S20 = ['S20','Cristofari_S20','Delcamp_S20']
-S21 = ['S21','Griessmeier_S21','Cecconi_S21','Bonne_S21','Brionne_S21']
+s00 = ['s00', 'Allen_s00','De_simone_s00','Deheuvels_s00','Leboulleux_s00','Metris_s00','Ng_s00', 'Noble_s00']
+s01 = ['s01', 'Cerardi_s01', 'Chu_S01', 'Kou_s01', 'Mahler_s01', 'Nagesh_s01', 'Pommiera_s01', 'Pommierb_s01']
+s02 = ['s02', 'Contorsi_s02', 'Fortin_s02', 'Khalil_s02', 'Li_s02', 'Martocchia_S02', 'Palicio_s02', 'Soubiran_s02', 'Tenachi_S02']
+s03 = ['s03', 'Barrere_s03', 'Berteaud_s03', 'Boughelilba_s03', 'Bretaudeau_s03', 'Bugli_s03', 'Decoene_s03'. 'Diez_s03', 'Duque_s03', 'Laviron_s03', 'Mottez_s03', 'Ng_s03', 'Petri_s03', 'Quintin_s03']
+s04 = ['s04', 'Buchlin_s04', 'Genova_s04']
+s05 = ['s05',]
+s06 = ['s06', 'Barroy_s06', 'Halbwachs_s06', 'Lekic_s06', 'Leroy_s06', 'Lilensten_s06', 'Midavaine_s06', 'Robineau_s06', 'Schuessler_s06']
+s07 = ['s07', 'Adam_s07', 'Farcy_s07']
+s08 = ['s08', 'Delsanti_s08', 'Puech_s08']
+s09 = ['s09', 'Pommier_s09']
+s10 = ['s10', 'Chastenet_s10', 'Correia_s10', 'Michoulier_S10', 'Milli_s10', 'Segretain_s10']
+s12 = ['s12', 'Acuna_s12', 'Chauvin_s12', 'Mouillet_s12']
+s13 = ['s13', 'Coutens_s13', 'Gavino_s13', 'Kessler_s13', 'Oers_s13', 'Valeille-Manet_s13']
+s14 = ['s14', 'Chastenet_s14', 'Gomez-Guijarro_s14', 'Paquereau_s14', 'Seille_s14']
+s15 = ['s15', 'Robert_s15', 'Rosat_s15', 'RoubeauTissot_s15', 'Staelen_s15']
+s16 = ['s16', 'Bergezcasalou_s16', 'Debatzdetrenquelleon_s16', 'Dhouib_s16', 'Fleury_s16', 'Huet_s16', 'Naar_s16', 'Priolet_s16', 'Robert_s16', 'Venot_s16']
+s17 = ['s17', 'Bessila_s17', 'Blondin_s17', 'Breton_s17', 'Cristofari_s17', 'Degott_s17', 'Dumont_s17', 'Faurobert_s17', 'Gonzalez_s17', 'Halbwachs_s17', 'Ligi_s17', 'Marinho_s17', 'Martel_s17', 'Martinod_s17', 'Mauxion_s17', 'Mignon-risse_s17', 'Monpribat_s17', 'Perraut_s17', 'Radureau_s17', 'Ravinet_s17']
+s18 = ['s18', 'Atteia_s18', 'Bugli_s18', 'Debony_s18', 'Fortin_s18', 'Foustoul_s18', 'Francois_s18', 'Mignon-risse_s18']
+s19 = ['s19', 'Adami_s19', 'Dennefeld_s19', 'Parra_Ramos_s19', 'Russell_s19', 'Schneider_s19', 'Soucail_S19', 'ThomsonParessant_s19']
+s20 = ['s20', 'Neiner_s20']
+s21 = ['s21', 'Leboulleux_s21']
 #WARNING : encoding of the ' -> \' and \'e -> \\\'e to keep latex text in the keys
 # comment out sessions with no contribution
 sessions = [
-    {'SF2A --- Plenary session (S0)':S0},
-    {'Atelier g\\\'en\\\'eral du PNHE (S1)':S1},
-    {'Atelier g\\\'en\\\'eral du PNPS (S2)':S2},
-    {'Atelier g\\\'en\\\'eral du PCMI (S3)':S3},
-    {'Education \`a l\'astronomie : structuration, activit\\\'es et ressources (S4)': S4},
-    {'Collaboration Pro-Am Gemini (S5)': S5},
-    {'L\'\\\'egalit\\\'e des genres en A\&A : point sur les jeunes (S6)': S6},
-    {'Transition environnementale : quel r\\^ole pour la communaut\\\'e astronomique ? (S7)': S7},
-    {'Mesures in-situ et en t\\\'el\\\'ed\\\'etection des plasmas du Syst\\`eme solaire (S9)': S9},
-    {'Dust in galaxies: from the local interstellar medium to distant galaxies (S10)': S10},
-    {'Le Soleil est-il une \\\'etoile de type solaire ? (S11)': S11},
-    {'Gravitational-wave astronomy and multi-messenger astrophysics (S13)': S13},
-    {'L\'astrophysique fran\\c{c}aise \\`a l\'heure du JWST : opportunit\\\'es et d\\\'efis (S14)':S14},
-    {'The Local Group in the Gaia era: from the Galactic halo to the Andromeda galaxy (S15)': S15},
-    {'Atmosph\\`eres d\'exoplan\\`etes dans le contexte du JWST (S16)': S16},
-    {'Disques de d\\\'ebris, exocom\\`etes et formation plan\\\'etaire (S18)': S18},
-    {'Cosmic turbulence (S20)': S20},
-    {'Le renouveau de la radio-astronomie et SKA, c\'est maintenant ! (S21)': S21}
+    {'SF2A --- Plenary session (S00)': s00},
+    {'Amas et groupes de galaxies dans l\'\`ere des grands relev\\\'es (S01)': s01},
+    {'Arch\\\'eologie Galactique avec Gaia/DR3: un an après (S02)': s02},
+    {'AS SKA-LOFAR and PNHE Workshop - Compact Objects: Observations and theory from radio to gamma (S03)': s03},
+    {'Open Science in Astronomy (S04)': s04},
+    {'Bien-\^etre en astrophysique (S05)': s05},
+    {'Partenariat Gemini SAF SF2A sur les collaborations amateurs professionnels (S06)': s06},
+    {'Cosmic Rays and Cosmology (S07)': s07},
+    {'Demain l\?ELT! Strat\\\'egies d\?observation, pr\\\'eparation des programmes scientifiques et synergies trans-instruments (S08)': s08},    
+    {'L\?\\\'egalit\\\'e des genres en A\&A: st\\\'er\\\'eotypes de genre dans l\?enseignement (S09)': s09},
+    {'Latest insights on dust evolution (S10)': s10},
+    {'Origine de l\'eau et de la vie: Quels d\\\'efis astrophysiques et instrumentaux pour demain ? (S12)': s12},
+    {'Atelier g\\\'en\\\'eral du PCMI (S13)': s13},
+    {'Galaxy evolution in the era of JWST (atelier du PNCG) (S14)': s14},
+    {'PNGRAM G\\\'eod\\\'esie Spatiale (S15)': s15},
+    {'Atelier g\\\'en\\\'eral du Programme National de Plan\\\'etologie (PNP) (S16)': s16},
+    {'Atelier g\\\'en\\\'eral du Programme National de Physique Stellaire (PNPS) (S17)': s17},
+    {'Pr\\\'ediction et suivi des signaux multi-messagers (S18)': s18},
+    {'Sixi\`eme r\\\'eunion des utilisateurs des t\\\'elescopes fran\{c}cais (TBL/OHP193) (S19)': s19},
+    {'The future of micro- and nanosatellites for astronomy (S20)': s20},
+    {'Transition environnementale : quels leviers d\'actions pour r\\\'eduire l\'empreinte environnementale de l\'astronomie ? (S21)': s21},
     ]
 
